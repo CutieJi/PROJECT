62340be03e1962041e1d15fc70f2879967f1e419
Rosh <roshjingel@gmail.com>
2024-10-18 15:57:28 +0800

    Add files via upload

---
+++ views/borrow/index.php
@@ -0,0 +1,13 @@
+
+
+        <!-- content -->
+
+        <div class="jumbotron mt-4">
+            <h1 class="display-4">Welcome to STYX SHELFHUB</h1>
+            <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum aliquid totam provident ducimus dolor consequuntur iure, maiores nihil, voluptate sunt laudantium exercitationem! Aperiam, quasi. Nesciunt, nobis. Corporis temporibus nesciunt deserunt odio ducimus excepturi eaque sapiente facere possimus, ratione quod nostrum non amet ipsam reprehenderit et pariatur iusto! Tenetur eius dignissimos veniam ad itaque dolore nulla consequuntur voluptates dolores molestiae. Quos, quidem error quia autem accusamus ut nemo, cum sequi tempora, doloremque harum incidunt voluptate ipsam laudantium doloribus! Reprehenderit aliquid suscipit inventore facere animi nam, non eveniet nostrum deserunt odit nulla perferendis voluptatum in quod voluptatem voluptas obcaecati odio velit impedit.</p>
+            <hr class="my-4">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate quibusdam deleniti harum impedit et ipsam nesciunt quae suscipit, aperiam cumque!</p>
+            <a class="btn btn-primary btn-lg" href="<?= site_url('Borrow') ?>" role="button">Borrow Now</a>
+        </div>
+
+
