31a4c6f2f664193b814dea49a734a493d6f4af12
Rosh <roshjingel@gmail.com>
2024-10-28 14:34:47 +0800

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:81/students_information/';
+$config['base_url'] = 'http://localhost:81/styx_shelfhub/';
 
 /*
 |--------------------------------------------------------------------------
