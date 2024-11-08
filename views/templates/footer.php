7d46a0aa60e5dac34bc8455ec275b4379ce3d503
Rosh <roshjingel@gmail.com>
2024-11-08 16:05:47 +0800

    Delete views directory

--- views/templates/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-
-
-<script src="<?= base_url('assets/js/jquery.js') ?>"></script>
-<script src="<?= base_url('assets/js/popper.js') ?>"></script>
-<script src="<?= base_url('assets/js/bootstrap.js') ?>"></script>
-</body>
-
-</html>
